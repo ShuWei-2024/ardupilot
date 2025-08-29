34c53abb86cc8c4cd0cf148932d2ef5c99735609
FJJ <fujiajun@shu.edu.sn>
2025-08-29 08:49:42 +0000

    refactor(AP_CompanionComputer): 重构数据包处理逻辑
    - 可以处理多种指令并发送反馈
    - 引入 PacketBuilder 类，用于数据包的序列化和反序列化
    - 使用array代替数组。constexpr代替define。优化数据接收和处理流程

--- libraries/AP_CompanionComputer/AP_CompanionComputer.cpp
+++ libraries/AP_CompanionComputer/AP_CompanionComputer.cpp
@@ -23,12 +23,14 @@ const AP_Param::GroupInfo AP_CompanionComputer::var_info[] = {
     AP_GROUPEND
 };
 
-AP_CompanionComputer::AP_CompanionComputer() 
-{
+AP_CompanionComputer::AP_CompanionComputer() :
+    _rx_state(RxState::WAITING_HEADER1),
+    _rx_count(0),
+    _uart(nullptr),
+    _log_c2hc_bit(-1) 
+    {
     AP_Param::setup_object_defaults(this, var_info);
-    _rx_state = RxState::WAITING_HEADER1;
-    _rx_count = 0;
-    _uart = nullptr;
+    _rx_buffer.fill(0);
 }
 
 void AP_CompanionComputer::init() 
@@ -50,10 +52,11 @@ void AP_CompanionComputer::update()
     if (!_enable || _uart == nullptr)
         return;
 
-    int16_t nbytes = _uart->available();
-    while (nbytes-- > 0) {
-        uint8_t byte = _uart->read();
-        process_received_data(byte);
+    while (_uart->available() > 0) {
+        uint8_t byte;
+        if (_uart->read(&byte, 1)) {
+            process_received_data(byte);
+        }
     }
 }
 
@@ -88,14 +91,13 @@ void AP_CompanionComputer::process_received_data(uint8_t oneByte)
         break;
 
     case RxState::WAITING_TYPE:
-        if(oneByte > 0x03){
+        if (oneByte <= 0x03) {
+            _rx_state = RxState::WAITING_LENGTH;
+            _rx_buffer[_rx_count++] = oneByte;
+            _cmd_type = oneByte;
+        } else {
             _rx_state = RxState::WAITING_HEADER1;
-            _rx_count = 0;
-            break;
         }
-        _rx_state = RxState::WAITING_LENGTH;
-        _cmd_type = oneByte;
-        _rx_buffer[_rx_count++] = oneByte;
         break;
 
     case RxState::WAITING_LENGTH:
@@ -116,20 +118,17 @@ void AP_CompanionComputer::process_received_data(uint8_t oneByte)
         // 检查是否接收到完整数据包（包括帧头、校验和、结束符）
         if (_rx_count >= (_data_len + 7)) {
             // 完整数据包接收，进行校验
-            if (validate_packet(_rx_buffer)) {
-                // 提取指令类型
-                _cmd_type = _rx_buffer[3]; // 指令类型在第4个字节
-                
+            if (validate_packet()) {
                 // 根据指令类型解析数据
                 switch (_cmd_type) {
                 case 0x01: // 飞行控制信息
-                    parse_flight_control_data(_rx_buffer);
+                    parse_flight_control_data();
                     break;
                 case 0x02: // 飞控参数设置
-                    parse_parameter_data(_rx_buffer);
+                    parse_parameter_data();
                     break;
                 case 0x03: // 系统控制
-                    parse_system_command(_rx_buffer);
+                    parse_system_command();
                     break;
                 default:
                     // 未知指令类型
@@ -145,29 +144,32 @@ void AP_CompanionComputer::process_received_data(uint8_t oneByte)
 }
 
 // 解析飞行控制数据
-void AP_CompanionComputer::parse_flight_control_data(uint8_t* buffer)
+void AP_CompanionComputer::parse_flight_control_data()
 {
-    memcpy(&_received_packet, buffer, sizeof(_received_packet));
-    _parsed_packet.ctrl_mode = _received_packet.ctrl_mode;
-    _parsed_packet.x_axis_err = _received_packet.x_axis_err;
-    _parsed_packet.y_axis_err = _received_packet.y_axis_err;
-    _parsed_packet.z_axis_err = _received_packet.z_axis_err;
-    _parsed_packet.max_velocity = float(_received_packet.max_velocity) / 100.0;
-    _parsed_packet.desired_yaw = float(_received_packet.desired_yaw) / 100.0;
-    _parsed_packet.target_lon = double(_received_packet.target_lon) / 1e7;
-    _parsed_packet.target_lat = double(_received_packet.target_lat) / 1e7;
-    _parsed_packet.target_alt = float(_received_packet.target_alt) / 100.0;
-    _parsed_packet.target_yaw = float(_received_packet.target_yaw) / 100.0;
-    _parsed_packet.target_velocity = float(_received_packet.target_velocity) / 100.0;
-    _parsed_packet.yaw_max_rate = float(_received_packet.yaw_max_rate) / 1000.0;
+    _received_packet =
+        PacketBuilder::deserialize<CompanionReceivePacket>(_rx_buffer.data());
+    _parsed_packet = {
+        .ctrl_mode = _received_packet.ctrl_mode,
+        .x_axis_err = _received_packet.x_axis_err,
+        .y_axis_err = _received_packet.y_axis_err,
+        .z_axis_err = _received_packet.z_axis_err,
+        .max_velocity = _received_packet.max_velocity / 100.0f,
+        .desired_yaw = _received_packet.desired_yaw / 100.0f,
+        .target_lon = _received_packet.target_lon / 1e7,
+        .target_lat = _received_packet.target_lat / 1e7,
+        .target_alt = _received_packet.target_alt / 100.0f,
+        .target_yaw = _received_packet.target_yaw / 100.0f,
+        .target_velocity = _received_packet.target_velocity / 100.0f,
+        .yaw_max_rate = _received_packet.yaw_max_rate / 1000.0f};
 
 #if HAL_LOGGING_ENABLED
     Log_C2HC();
 #endif
+    
 }
 
 // 解析参数设置数据（待实现）
-void AP_CompanionComputer::parse_parameter_data(uint8_t *buffer)
+void AP_CompanionComputer::parse_parameter_data()
 {
     // 根据具体参数格式实现解析逻辑
     // 示例：提取参数索引和值
@@ -179,10 +181,10 @@ void AP_CompanionComputer::parse_parameter_data(uint8_t *buffer)
 
 
 // 解析系统控制命令
-void AP_CompanionComputer::parse_system_command(uint8_t* buffer)
+void AP_CompanionComputer::parse_system_command()
 {
     // 提取命令数据（假设命令数据在第四个字节）
-    uint8_t cmd_data = buffer[3];
+    uint8_t cmd_data = _rx_buffer[5];
     uint8_t status = 0x01;
     switch (cmd_data) {
     case 0x02: // 重启
@@ -193,23 +195,23 @@ void AP_CompanionComputer::parse_system_command(uint8_t* buffer)
         break;
     default:
         // 未知命令
-        send_response(0xFF, 0x02);
+        status = 0x02;
         break;
     }
     send_response(cmd_data, status);
 }
 
-// 校验数据包（需要根据协议实现）TODO：要根据长度改
-bool AP_CompanionComputer::validate_packet(const uint8_t *buffer) const
+// 校验数据包（需要根据协议实现）
+bool AP_CompanionComputer::validate_packet() const
 {
     // 计算校验和
     // 校验和在倒数第二个字节，结束符在最后一个字节
     uint8_t calculated_checksum = 0;
     for (int i = 0; i < _rx_count - 2; i++) {
-        calculated_checksum += buffer[i];
+        calculated_checksum += _rx_buffer[i];
     }
 
-    return (calculated_checksum == buffer[_rx_count - 2]) && (buffer[_rx_count - 1] == 0xFF);
+    return (calculated_checksum == _rx_buffer[_rx_count - 2]) && (_rx_buffer[_rx_count - 1] == 0xFF);
 }
 
 void AP_CompanionComputer::send_data() 
@@ -252,7 +254,7 @@ void AP_CompanionComputer::send_data()
     if (ahrs.get_location(loc)) {
         pkt.my_lon = loc.lng;
         pkt.my_lat = loc.lat;
-        pkt.my_alt = loc.alt / 100; // cm转m
+        pkt.my_alt = loc.alt; // cm转m
     }
     // pkt.my_alt = _parsed_packet.x_axis_err;         //debug，务必删除
 
@@ -269,19 +271,19 @@ void AP_CompanionComputer::send_data()
     pkt.my_yaw   = degrees(att.z) * 100;
     
     // 计算校验和
-    pkt.checksum = calculate_checksum(reinterpret_cast<uint8_t*>(&pkt), COMPANION_SEND_TOTAL_LENGTH-2); // byte0-8
-    pkt.end_sign = COMPANION_END_SIGN;
+    auto packet = PacketBuilder::serialize(pkt);
+    packet[packet.size()-2] = calculate_checksum(packet.data(), packet.size()-2);
+    packet[packet.size()-1] = COMPANION_END_SIGN;
 
     // 发送数据
-    _uart->write((const uint8_t*)&pkt, sizeof(pkt));
+    _uart->write(packet.data(), packet.size());
     _last_sent_ms = now;
 }
 
 void AP_CompanionComputer::send_response(uint8_t data, uint8_t status)
 {
     // 计算数据包总长度：同步字节(2) + 指令来源(1) + 指令类型(1) + 数据长度(1) + 数据(2) + 校验位(1) + 结束位(1)
-    const uint8_t packet_length = 9;
-    uint8_t response_buffer[packet_length];
+    std::array<uint8_t, COMPANION_SEND_RESP_LENGTH> response_buffer;
     response_buffer[0] = COMPANION_FRAME_HEADER1; // 同步字节1
     response_buffer[1] = COMPANION_FRAME_HEADER2; // 同步字节2
     response_buffer[2] = COMPANION_CMD_SOURCE_FC; // 来自FCU
@@ -290,17 +292,12 @@ void AP_CompanionComputer::send_response(uint8_t data, uint8_t status)
     response_buffer[5] = data;   // 收到的指令数据
     response_buffer[6] = status; // 执行状态
     // 计算校验和（从同步字节到数据结束）
-    uint8_t checksum = 0;
-    for (int i = 0; i < 7; i++)
-    {
-        checksum += response_buffer[i];
-    }
-    response_buffer[7] = checksum; // 校验位
+    response_buffer[7] = calculate_checksum(response_buffer.data(), response_buffer.size() - 2);
     // 填充结束位
     response_buffer[8] = 0xFF; // 结束符
 
     // 通过串口发送数据
-    _uart->write(response_buffer, packet_length);
+    _uart->write(response_buffer.data(), response_buffer.size());
 }
 
 uint8_t AP_CompanionComputer::calculate_checksum(const uint8_t *data, uint8_t len) const 
